851ecbfd5f56e4546ed1058822c9066bc12c1648
Akio K <akiokaki@umich.edu>
2017-10-16 16:41:54 -0400

    Complete Part 0 and 1

--- si507f17_project3_code.py
+++ si507f17_project3_code.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as Soup
 import unittest
 import requests
 
@@ -13,8 +13,25 @@ import requests
 ######### PART 0 #########
 
 # Write your code for Part 0 here.
+should_I_cache = 1
+if(should_I_cache == 1):
+    try: 
+        cats_page = open('cats_page.html','r').read()
+    except:
+        cats_page = requests.get('http://newmantaylor.com/gallery.html').text
+        f = open('cats_page.html','w')
+        f.write(cats_page)
+        f.close()
+else: pass
 
-
+cat_soup = Soup(cats_page, 'html.parser')
+cat_img_repository = cat_soup.find_all("img")
+#print(type(cat_img_repository))
+cat_alt = []
+for cat_element in cat_img_repository:
+    cat_alt.append(cat_element.get('alt',"No alternative text provided!"))
+print(cat_alt)
+    
 ######### PART 1 #########
 
 # Get the main page data...
@@ -27,9 +44,17 @@ import requests
 
 # We've provided comments to guide you through the complex try/except, but if you prefer to build up the code to do this scraping and caching yourself, that is OK.
 
-
-
-
+try: 
+    main_page_data = open('nps_gov_data.html','r').read()
+except:
+    main_page_data = requests.get('https://www.nps.gov/index.htm')
+    main_page_data.encoding = 'utf-8'
+    # print('The main_page_data is encoded in: {}'.format(main_page_data.encoding))
+    # print('The main_page_data is type: {}'.format(type(main_page_data)))
+    main_page_data = main_page_data.text
+    f = open('nps_gov_data.html','w')
+    f.write(main_page_data)
+    f.close()
 
 
 # Get individual states' data...
@@ -69,10 +94,75 @@ import requests
 # And then, write each set of data to a file so this won't have to run again.
 
 
+try:
+    # To open and read all 3 of the files
+    arkansas_data = open('arkansas_data.html').read()
+    california_data = open('california_data.html').read()
+    michigan_data = open('michigan_data.html').read()
+except:
+    # Create a BeautifulSoup instance of main page data
+    main_page_soup = Soup(main_page_data,'html.parser')
+    
+    # Access the unordered list with the states' dropdown
+    main_page_soup_stateUl = main_page_soup.find("ul",{"class":"dropdown-menu SearchBar-keywordSearch"})
+    
+    # prettify the main nps page
+    f = open("prettified_main_page.html",'w')
+    f.write(main_page_soup_stateUl.prettify())
+    f.close
+    
+    # Get a list of all the li (list elements) from the unordered list, using the BeautifulSoup find_all method
+    state_href_list = []
+    li_main_page = main_page_soup_stateUl.find_all("li")
+    for e in li_main_page:
+        state_href_list.append(e)
+    #print(state_href_list)
+    
+    # Use a list comprehension or accumulation to get all of the 'href' attributes of the 'a' tag objects in each li, instead of the full li objects
+    href_list = [x['href'] for x in main_page_soup_stateUl.find_all('a')]
+    #print(href_list)
 
+    # Filter the list of relative URLs you just got to include only the 3 you want: AR's, CA's, MI's, using the accumulator pattern & conditional statements
+    target_states = ['ar','ca','mi']
+    #target_states = [i+'/index.htm' for i in target_states]
+    #print(target_states)
 
+    target_href_dict = {}
+    for i in target_states:
+        target_href_dict[i] = '/state/'+i+'/index.htm' 
+    #print(target_href_dict)
 
+    # Create 3 URLs to access data from by appending those 3 href values to the main part of the NPS url. Save each URL in a variable.
+    
+    main_url = "https://www.nps.gov"
+    ar_url = main_url+target_href_dict['ar']
+    ca_url = main_url+target_href_dict['ca']
+    mi_url = main_url+target_href_dict['mi']
+    print(ar_url)
+    print(ca_url)
+    print(mi_url)
+    # Finally, get the HTML data from each of these URLs, and save it in the variables you used in the try clause
+    # (Make sure they're the same variables you used in the try clause! Otherwise, all this code will run every time you run the program!)
 
+    ar_data = requests.get(ar_url)
+    ar_data.encoding = 'utf-8'
+    ar_data = ar_data.text
+    with open("arkansas_data.html", 'w') as f:
+        f.write(ar_data)
+
+    ca_data = requests.get(ca_url)
+    ca_data.encoding = 'utf-8'
+    ca_data = ca_data.text
+    with open("california_data.html", 'w') as f:
+        f.write(ca_data)
+
+    mi_data = requests.get(mi_url)
+    mi_data.encoding = 'utf-8'
+    mi_data = mi_data.text
+    with open("michigan_data.html", 'w') as f:
+        f.write(mi_data)    
+
+    # And then, write each set of data to a file so this won't have to run again.
 
 ######### PART 2 #########
 
@@ -96,17 +186,33 @@ import requests
 
 
 ## Define your class NationalSite here:
-
-
-
-
-
+class NationalSite(object):
+    def __init__(self, state_Soup):
+        self.location = state_Soup.find("h4")
+        self.name = state_Soup.find("h3")
+        if state_Soup.find("h2") is not None:
+            self.type = state_Soup.find("h2")
+        else: 
+            self.type = None
+        if state_Soup.find("p") is not None:
+            self.description = state_Soup.find("p")
+        else:
+            self.description = ""
+    def __str__(self):
+        return "**{} | {}**".format(self.name, self.location)
+    def __contains__(self, test_string):
+        return test_string in self.title
+    def get_mailing_address(self):
+        pass
+    
 ## Recommendation: to test the class, at various points, uncomment the following code and invoke some of the methods / check out the instance variables of the test instance saved in the variable sample_inst:
 
-# f = open("sample_html_of_park.html",'r')
-# soup_park_inst = BeautifulSoup(f.read(), 'html.parser') # an example of 1 BeautifulSoup instance to pass into your class
-# sample_inst = NationalSite(soup_park_inst)
-# f.close()
+f = open("sample_html_of_park.html",'r')
+soup_park_inst = Soup(f.read(), 'html.parser') # an example of 1 BeautifulSoup instance to pass into your class
+sample_inst = NationalSite(soup_park_inst)
+f.close()
+
+
 
 
 ######### PART 3 #########
